84db56317d0beb010ef95908e3dca59b01baa6f9
alafsasa <alafsasa@gmail.com>
2023-02-13 21:52:22 +0300

    stripped down

--- src/App.js
+++ src/App.js
@@ -2,64 +2,61 @@ import React, {useEffect, useRef} from 'react';
 import './App.css';
 import { fabric } from 'fabric';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowPointer, faBucket, faCircle, faDrawPolygon, faEraser, faImage, faPen, faSave, faSquare, faTextWidth, faTrash, faTriangleExclamation } from '@fortawesome/free-solid-svg-icons';
+import { faSquare } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   //const [isRectActive, setIsRectActive] = useState(false);
   const kanvas = useRef();
-  var isRectActive = false;
   //configure flags
   useEffect(()=>{
     var paper = new fabric.Canvas(kanvas.current, {backgroundColor: '#ddd'});
     var isDown = false;
-    var origX, origY, rect;
-    //mousedown
+    var originX, originY, rect = null;
+    //mouse down
     paper.on('mouse:down', (o)=>{
+      //get mouse pos
       var pointer = paper.getPointer(o.e);
       //console.log(pointer)
-      isDown = true;
-      origX = pointer.x;
-      origY = pointer.y;
-      console.log(isRectActive)
-      //check if rectangle is active
-      if(isRectActive === true){
-        rect = new fabric.Rect({
-          left: origX,
-          top: origY,
-          fill: '',
-          stroke: 'red',
-          strokeWidth: 2
-        });
-        paper.add(rect);
-      }
+      originX = pointer.x;
+      originY = pointer.y;
+      //set is down flag to 1
+      isDown = true
+      //init rect
+      rect = new fabric.Rect({
+        left: originX,
+        top: originY,
+        fill:'',
+        stroke: 'red',
+        strokeWidth: 2,
+      });
+      paper.add(rect);
     });
-    //mousemove
+    //mouse move/drag
     paper.on('mouse:move', (o)=>{
-      if(!isDown){return;}
-      var pointer = paper.getPointer(o.e);
-      //check is rect is active
-      if(isRectActive === true){
-        if(origX > pointer.x){
-          rect.set({left: Math.abs(pointer.x)});
-        }
-        if(origY > pointer.y){
-          rect.set({top: Math.abs(pointer.y)});
-        }
-        //otherwise
-        rect.set({width: Math.abs(origX - pointer.x)});
-        rect.set({height: Math.abs(origY - pointer.y)});
-        paper.renderAll();
+      if(!isDown){
+        return;
       }
+      //else
+      //get pointer pos
+      var pointer = paper.getPointer(o.e);
+      //compare
+      if(originX > pointer.x){
+        rect.set({left: Math.abs(pointer.x)});
+      }
+      if(originY > pointer.y){
+        rect.set({top: Math.abs(pointer.y)});
+      }
+      //otherwise
+      rect.set({width: Math.abs(originX - pointer.x)});
+      rect.set({height: Math.abs(originY - pointer.y)});
+      paper.renderAll();
     });
     //mouse up
     paper.on('mouse:up', (o)=>{
       isDown = false;
-    });
-  }, [isRectActive]); //the second empty array ensure we execute the function only once
-  //set flag functions
-  function activateRect(){
-    isRectActive = true;
-  }
+    })
+    //rect
+  }, []); //the second empty array ensure we execute the function only once
   return (
     <div className="container">
       <div className='row'>
@@ -67,41 +64,8 @@ function App() {
           <div className='display-4 text-center'>FabricJs - Meme Creator</div>
           <canvas ref={kanvas} className="cv" height={700} width={700}></canvas>
           <div className='mt-3 bg-info'>
-            <button className='btn' onClick={activateRect}>
+            <button className='btn'>
               <FontAwesomeIcon icon={faSquare} size="2x" /> Rectangle/Square
-            </button>
-            <button className='btn'>
-              <FontAwesomeIcon icon={faTriangleExclamation} size="2x" /> Triangle
-            </button>
-            <button className='btn'>
-              <FontAwesomeIcon icon={faCircle} size="2x" /> Circle
-            </button>
-            <button className='btn'>
-              <FontAwesomeIcon icon={faPen} size="2x" /> Pen
-            </button>
-            <button className='btn'>
-              <FontAwesomeIcon icon={faTextWidth} size="2x" /> Text
-            </button>
-            <button className='btn'>
-              <FontAwesomeIcon icon={faImage} size="2x" /> Add Image
-            </button>
-            <button className='btn'>
-              <FontAwesomeIcon icon={faEraser} size="2x" /> Erase
-            </button>
-            <button className='btn'>
-              <FontAwesomeIcon icon={faBucket} size="2x" /> Color
-            </button>
-            <button className='btn'>
-              <FontAwesomeIcon icon={faDrawPolygon} size="2x" /> Polygon
-            </button>
-            <button className='btn'>
-              <FontAwesomeIcon icon={faTrash} size="2x" /> Delete
-            </button>
-            <button className='btn'>
-              <FontAwesomeIcon icon={faArrowPointer} size="2x" /> Select
-            </button>
-            <button className='btn'>
-              <FontAwesomeIcon icon={faSave} size="2x" /> Save
             </button>
           </div>
         </div>
