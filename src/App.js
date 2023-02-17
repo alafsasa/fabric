71e6b34e3a92efefedb1e6d27d138835e6584251
alafsasa <alafsasa@gmail.com>
2023-02-17 22:21:11 +0300

    managed to expose the canvas and able to manipulate from handlers

--- src/App.js
+++ src/App.js
@@ -1,100 +1,75 @@
-import React, { useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 import { fabric } from 'fabric';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
-  //const [paper, setPaper] = useState('');
-  const kanvas = useRef(null)
-  //const [isRectActive, setIsRectActive] = useState(false);
-  //const [toggle, setToggle] = useState(false);
-  //const kanvas = useRef();
-  //const y = useRef();
-  //y.current = isRectActive;
-  //configure flags
-  useEffect(()=>{
-    console.log('render')
-    var paper = new fabric.Canvas(kanvas.current, {backgroundColor: '#ddd'});
-    var isDown, objSelected = false;
-    var originX, originY, rect, sl = null;
-    
-    //mouse down
-    paper.on('mouse:down', (o)=>{
-      //get mouse pos
-      var pointer = paper.getPointer(o.e);
-      //console.log(pointer)
-      originX = pointer.x;
-      originY = pointer.y;
-      //set is down flag to 1
-      isDown = true
-      //if object is selected
-      sl = paper.getActiveObject();
-      if(sl === undefined || sl === null){
-        console.log('no objects!')
-        objSelected = false;
-      }else{
-        objSelected = true;
-      }
-      if(objSelected === false){
-          //init rect
-          rect = new fabric.Rect({
-            left: originX,
-            top: originY,
-            fill:'',
-            stroke: 'red',
-            strokeWidth: 2,
-          });
-          paper.add(rect);
-        }
-    });
-    //mouse move/drag
-    paper.on('mouse:move', (o)=>{
-      if(!isDown){
-        return;
-      }
-      //else
-      //get pointer pos
-      var pointer = paper.getPointer(o.e);
-      if(objSelected === false){
-              //compare
-      if(originX > pointer.x){
-        rect.set({left: Math.abs(pointer.x)});
-      }
-      if(originY > pointer.y){
-        rect.set({top: Math.abs(pointer.y)});
-      }
-      //otherwise
-      rect.set({width: Math.abs(originX - pointer.x)});
-      rect.set({height: Math.abs(originY - pointer.y)});
-      paper.renderAll();
-      }
-    });
-    //mouse up
-    paper.on('mouse:up', (o)=>{
-      isDown = false;
-    });
-    //unit test
-  }, []); //the second empty array ensure we execute the function only once
-  //delete objects
-  const deleteObjects = () => {
-    //logic
-    //console.log(paper)
+  const [cv, setCv] = useState(null);
+  const kanvas = useRef();
+
+  function someFunc(x){
+    //console.log(x)
+    setCv(x);
   }
-  const testMe = () => {
-    //logic
+  //logic
+  useEffect(()=>{
+    var paper = new fabric.Canvas(kanvas.current, {backgroundColor: '#ddd'});
+    //paper.add(new fabric.Rect({top: 100, left: 100, width: 100, height: 100, fill: 'red'}));
+    someFunc(paper);
+
+    //mousedown
+    console.log('tracker - x1')
+    paper.on('mouse:down', (o)=>{
+      console.log('mouse down');
+    });
+    //mouse move
+    //paper.on('mouse:move', (o)=>{
+    //  console.log('mouse moving...🚶‍♂️');
+    //})
+    //mouseup
+    paper.on('mouse:up', (o)=>{
+      console.log('out');
+    })
+
+
+    //clear canvas
+    return () => {
+      if(paper){
+        paper.dispose();
+        paper = undefined;
+      }
+    }
+  }, []);
+  function drawRect(){
+    const rect = new fabric.Rect({
+      top: 100,
+      left: 100,
+      width: 100,
+      height: 100,
+      fill: 'red'
+    });
+    cv.add(rect)
+  }
+  function deleteObj(){
+    console.log(cv)
+  }
+  function showSelectedObj(){
+    console.log(cv.getActiveObject())
+  }
+  function purgeObj(){
+    cv.remove(cv.getActiveObject());
   }
   return (
     <div className="container">
       <div className='row'>
         <div className='col-sm-12'>
-          <div className='display-4 text-center'>FabricJs - Meme Creator</div>
-          <canvas ref={kanvas} width={700} height={700} className="cv"></canvas>
-          <div className='mt-3 bg-info' onClick={deleteObjects}>
-            <button className='btn'>
-              <FontAwesomeIcon icon={faTrashAlt} size="2x" /> Delete objects
-            </button>
-            <button onClick={testMe}>test</button>
+          <div className='display-3'>FabricJs Meme Creator</div>
+          <canvas ref={kanvas} width={500} height={500}></canvas>
+          <div className='mt-3'>
+            <button className='btn btn-primary' onClick={deleteObj}>Click me</button>
+            <button className='btn btn-warning' onClick={drawRect}>Add Rect</button>
+            <button className='btn btn-info' onClick={showSelectedObj}>Active</button>
+            <button className='btn btn-danger' onClick={purgeObj}>Delete</button>
           </div>
         </div>
       </div>
